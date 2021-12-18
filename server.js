88ca614b5cec9bc2a20629ff21bd9cfa68158f59
Nazar <nazariykharchuk@gmail.com>
2021-12-18 23:34:22 +0200

    added the first page

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log('Hello, world!)');
+var express = require('express');
+
+var app = express();
+
+app.use('/static', express.static('static'));
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + "/index.html")
+});
+
+app.listen(3000);
